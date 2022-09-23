01ae019724e16234a3f939eebe78532b12f37845
Matthias <herzer.matthias@gmail.com>
2022-09-23 18:13:31 +0200

    Rechner mit rudimäntärem Additionsverfahren

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        int variable_1 = 2;
+        int variable_2 = 4;
+        int result;
+        String name ="Additonator 3000";
+
+        result=variable_1+variable_2;
+        System.out.println("This is the fabulous"+name);
+        System.out.print(variable_1+" + ");
+        System.out.print(variable_2+" = ");
+        System.out.print(result);
+
+    }
+}
