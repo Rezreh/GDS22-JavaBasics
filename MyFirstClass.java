184779664939d8f12702cdb17678b3dd43c9d3d5
Matthias <herzer.matthias@gmail.com>
2022-09-23 17:05:20 +0200

    Erste Übung, Erstellung und erklärung von println

---
+++ MyFirstClass.java
@@ -0,0 +1,10 @@
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Insert thoughtful Text here");
+        System.out.println(args);
+
+
+    }
+}
